6cdaa786ee7b7c178485bc22d1a78a63d265622e
Ivan Bonk <vaaeking@gmail.com>
2018-11-27 20:31:35 +0000

    add next page

--- js/index.js
+++ js/index.js
@@ -1,62 +1,105 @@
 let dayValue = 0; 
-let classValue;
+let courseValue;
+let groupValue;
 $('.day').click(dayChoise);
-$('.classNum').click(classChoise);
+$('.courseNum').click(courseChoise);
 $('.access').click(accessChoise);
-
-
 
 
 function dayChoise(event){
     const target = event.target;
-    dayValue = target.name
+    dayValue = target.name;
     $('.dayBtn').text(target.innerText); 
 }
 
-function classChoise(event) {
+function courseChoise(event) {
     const target = event.target;
-    classValue = target.name;
-    $('.classBtn').text(target.innerText);
-    
-    const courseList = new LessonList('')
+    courseValue = target.name;
+    $('.courseBtn').text(target.innerText);
+    let action = true;
+    const courseList = new LessonList('lessons.json', $('.render_dropdown'), action)
+}
+
+function groupChoise(event){
+    const target = event.target;
+    groupValue = target.name;
+    $('.numberBtn').text(target.innerText);
 }
 
 function accessChoise(){
-    const lessonList = new LessonList('lessons.json', $('tbody'));
+    let action = false;
+    const lessonList = new LessonList('lessons.json', $('tbody'), action);
 }
 
 class LessonList {
-    constructor (lessonsUrl, renderContainer) {
+    constructor (lessonsUrl, renderContainer, action) {
         fetch(lessonsUrl)
             .then(result => result.json() )
             .then(products => {
                 this.products = products;
-                this.renderProducts(renderContainer, products);
+                if(action) {
+                    this.renderDropdown(renderContainer, products)
+                }
+                else this.renderProducts(renderContainer, products);
             })
     }
    
-    renderProducts(container, products,) {
+    renderProducts(container, products) {
         let productListDomString = '';
-        
-        console.log(products);
-        
+
         products.forEach(product => {
           
-            if(product.class === classValue){
-                if(product.day === +dayValue){
-                    
-                    productListDomString +=
-                        `<tr>
-                          <th scope="row">${product.lesson}</th>
-                          <td>${product.name}</td>
-                          <td>${product.teacher}</td>
-                          <td>${product.room}</td>
-                        </tr>`;
+            if(product.course === +courseValue){
+                if(product.group === groupValue){
+                    if(product.day === +dayValue){
+                        productListDomString +=
+                            `<tr>
+                              <th scope="row">${product.lesson}</th>
+                              <td>${product.name}</td>
+                              <td>${product.teacher}</td>
+                              <td>${product.room}</td>
+                            </tr>`;
+                    }
                 }
             }    
         });
         $('table').css('display', 'table');
         container.html(productListDomString);
     }
+    
+    renderDropdown(container, products) {
+        let productListDomString = '';
+        
+        let curr;
+        products.forEach(product => {
+            if(product.course === +courseValue){
+                if(product.group != curr){
+                    curr = product.group;
+                productListDomString +=
+                    `<a class="dropdown-item groupName" name="${product.group}">${product.group}</a>`;
+            
+                }
+            }    
+        });
+        container.html(productListDomString);
+        $('.groupName').click(groupChoise);
+    }
 }
 
+
+$('td').click(pressTable);
+$('.addTeacherBtn').click(showTable);
+$('.nextTeacher').click(nextTeacher);
+
+function pressTable(event) {
+    const target = event.target;
+    $(target).toggleClass('BlueBG');
+}
+
+function showTable(){
+    $('.addTeacher').toggle();
+}
+
+function nextTeacher(){
+        
+}
